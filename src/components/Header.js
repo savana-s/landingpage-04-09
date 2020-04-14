d4b12e7fec96f957e9b0236f73068b3e1c7d48e3
savana-s <savanasathre@gmail.com>
2020-04-14 10:40:27 -0700

    3.81

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,12 @@ export default class Header extends Component {
         <header id="home">
           <div className="row banner">
             <div className="banner-text">
-              <h1 className="responsive-headline">🌑</h1>
+              <h1 className="responsive-headline"></h1>
               <h1 className="responsive-headline">I AM</h1>
               <br />
-              <h2 style={{ color: "#fff", fontFamily: "sans-serif " }}>
-                I KNOW WHO I AM
-              </h2>
-              <h2 style={{ color: "#fff", fontFamily: "sans-serif " }}>
-                I KNOW WHAT I AM DOING
-              </h2>{" "}
-              <h2 style={{ color: "#fff", fontFamily: "sans-serif " }}>
-                I KNOW HOW I AM FEELING
-              </h2>{" "}
-              <h2 style={{ color: "#fff", fontFamily: "sans-serif " }}>
-                I KNOW WHEN AND WHERE I AM
-              </h2>
-              <hr />
+              <h2>I KNOW WHO I AM</h2>
+              <h2>I KNOW WHAT I AM DOING</h2> <h2>I KNOW HOW I AM FEELING</h2>{" "}
+              <h2>I KNOW WHEN AND WHERE I AM</h2>
             </div>
           </div>
         </header>
