4b3f23f107a4cb6c7aa5f612c679d35990dfa35a
savana-s <savanasathre@gmail.com>
2020-04-13 22:14:14 -0700

    0.35
    App link finished.
    
    Need to do:
    Header, diamond indent paragragh, 3 components with ios screenshot

--- src/components/About2.js
+++ src/components/About2.js
@@ -4,17 +4,29 @@ export default class About2 extends Component {
     return (
       <section id="about2">
         <div className="columns contact-details">
-          <h2 style={{ fontSize: "1.2em" }}>Welcome to USEIAM</h2>
-          <p style={{ fontSize: "0.8em" }}>
+          <h2>Welcome to USEIAM</h2>
+          <p>
             USEIAM allows me to use my smart phone to guide me every day. It's
             like having a shadow in my phone.
           </p>
           <div class="row">
-            <div class="column">
-              <img src="https://freeiconshop.com/wp-content/uploads/edd/app-store-badge.png" />
+            <div class="column1">
+              <a
+                href="https://apps.apple.com/us/app/useiam/id1497407740"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="images/appstore.png" alt="appstore" />
+              </a>
             </div>
-            <div class="column">
-              <img src="https://freeiconshop.com/wp-content/uploads/edd/google-play-badge.png" />{" "}
+            <div class="column2">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.useiam.useiam2"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="images/googleplay.png" alt="googlestore" />{" "}
+              </a>
             </div>
           </div>
         </div>
--- src/components/Ninth.js
+++ src/components/Ninth.js
@@ -4,16 +4,27 @@ export default class Ninth extends Component {
   render() {
     return (
       <section id="ninth">
-        {" "}
         <div className="columns contact-details">
-          <h2>Try USEIAM Today</h2>{" "}
-          <div class="flex social-btns">
-            <a class="app-btn blu flex vert" href="http:apple.com">
-              <img src="https://freeiconshop.com/wp-content/uploads/edd/app-store-badge.png" />
-            </a>
-            <a class="app-btn blu flex vert" href="http:google.com">
-              <img src="https://freeiconshop.com/wp-content/uploads/edd/google-play-badge.png" />{" "}
-            </a>
+          <h2>Try USEIAM Today</h2>
+          <div class="row">
+            <div class="column1">
+              <a
+                href="https://apps.apple.com/us/app/useiam/id1497407740"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="images/appstore.png" alt="appstore" />
+              </a>
+            </div>
+            <div class="column2">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.useiam.useiam2"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src="images/googleplay.png" alt="googlestore" />{" "}
+              </a>
+            </div>
           </div>
         </div>
       </section>
